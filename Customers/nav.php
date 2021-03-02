4df51bffb2c12284e8c411938197112aca305d8b
AmitManna99 <manna.amit99@gmail.com>
2021-03-02 17:10:27 +0530

    visual refresh

--- Customers/nav.php
+++ Customers/nav.php
@@ -7,12 +7,13 @@
 	 <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon" />
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/local.css" />
+    
 
     <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
     <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 
-   
+    <link rel="stylesheet" type="text/css" href="css/local.css" />
+    <link rel="stylesheet" href="css/style.css">
     
 </head>
 <body>
@@ -28,39 +29,43 @@
                 <a class="navbar-brand" href="index.php">Urban Services</a>
             </div>
             <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li><a href="index.php"> &nbsp; <span class='glyphicon glyphicon-home'></span> Home</a></li>
-					<li class="active"><a href="shop.php?id=1"> &nbsp; <span class='glyphicon glyphicon-shopping-cart'></span>Book Now</a></li>
-					<li><a href="cart_items.php"> &nbsp; <span class='fa fa-cart-plus'></span> Order Cart </a></li>
-					<li><a href="orders.php"> &nbsp; <span class='glyphicon glyphicon-list-alt'></span> My Booked Services</a></li>
-					<li><a href="view_purchased.php"> &nbsp; <span class='glyphicon glyphicon-eye-open'></span> Order History</a></li>
-					<li><a data-toggle="modal" data-target="#setAccount"> &nbsp; <span class='fa fa-gear'></span> Account Settings</a></li>
-					<li><a href="logout.php"> &nbsp; <span class='glyphicon glyphicon-off'></span> Logout</a></li>
-					
-                    
+                <!-- LEFT NAV  -->
+                <div class="navbar-left">
+                <ul class="navbar-nav-left" id="menu_bar">
+                    <li class="nav-item"><a class="nav-link" href="index.php"> &nbsp; <i class='glyphicon glyphicon-home'></i><span class="link-text"> Home</span></a></li>
+					<li class="nav-item"><a class="nav-link" href="shop.php?id=1"> &nbsp; <i class='glyphicon glyphicon-shopping-cart'></i><span class="link-text"> Book Now</span></a></li>
+					<li class="nav-item"><a class="nav-link" href="cart_items.php"> &nbsp; <i class='fa fa-cart-plus'></i><span class="link-text"> Order Cart</span></a></li>
+					<li class="nav-item"><a class="nav-link" href="orders.php"> &nbsp; <i class='glyphicon glyphicon-list-alt'></i><span class="link-text"> My Booked Services</span></a></li>
+					<li class="nav-item"><a class="nav-link" href="view_purchased.php"> &nbsp; <i class='glyphicon glyphicon-eye-open'></i><span class="link-text"> Order History</span></a></li>
+					<li class="nav-item"><a class="nav-link" data-toggle="modal" data-target="#setAccount"> &nbsp; <i class='fa fa-gear'></i><span class="link-text"> Account Settings</span></a></li>
+					<li class="nav-item"><a class="nav-link" href="logout.php"> &nbsp; <i class='glyphicon glyphicon-off'></i><span class="link-text"> Logout</span></a></li>                    
                 </ul>
+                </div>
+               
+
+                <!-- TOP NAV -->
                 <ul class="nav navbar-nav navbar-right navbar-user">
-                    <li class="dropdown messages-dropdown">
+                    <!-- <li class="dropdown messages-dropdown">
                         <a href="#"><i class="fa fa-calendar"></i>  <?php
-                            $Today=date('y:m:d');
-                            $new=date('l, F d, Y',strtotime($Today));
-                            echo $new; ?></a>
+                            // $Today=date('y:m:d');
+                            // $new=date('l, F d, Y',strtotime($Today));
+                            // echo $new; ?></a>
                         
-                    </li>
+                    </li> -->
 					<li class="dropdown user-dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class='glyphicon glyphicon-shopping-cart'></span> Total Price Ordered: &#8369; <?php echo $total; ?> </b></a>
                        
                     </li>
 					
 					
-                     <li class="dropdown user-dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $user_email; ?><b class="caret"></b></a>
+                     <!-- <li class="dropdown user-dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php //echo $user_email; ?><b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li><a data-toggle="modal" data-target="#setAccount"><i class="fa fa-gear"></i> Settings</a></li>
                             <li class="divider"></li>
                             <li><a href="logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
                         </ul>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </nav>
--- index.php
+++ index.php
@@ -12,7 +12,19 @@ session_start();
     <title>Urban Services</title>
 	 <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon" />
 
-
+   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
 	<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
 	<link href="assets/css/flexslider.css" rel="stylesheet" />
@@ -66,8 +78,8 @@ session_start();
 	   
 	   
 	   
-	   
-       <!--HOME SECTION END-->   
+<!-- 	   
+       
     <div  class="tag-line" >
          <div class="container">
            <div class="row  text-center" >
@@ -80,7 +92,7 @@ session_start();
              </div>
         
     </div>
-    <!--HOME SECTION TAG LINE END-->   
+   
 	<div  class="tag-line" >
          <div class="container">
            <div class="row  text-center" >
@@ -93,7 +105,7 @@ session_start();
              </div>
         
     </div>
-    <!--HOME SECTION TAG LINE END-->   
+        -->
      
 		
 		
@@ -224,6 +236,347 @@ session_start();
           </div>
         </div>
 		
+
+
+        <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Prakash Shahi</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="#">Hire me</a>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="row_one">
+            <div class="services">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fa fa-scissors" aria-hidden="true"></i>
+                                <i class="fa fa-scissors" aria-hidden="true"></i>
+                            </div>
+                            <div class="name" data-text="Salon">Salon</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fa fa-tint" aria-hidden="true"></i>
+                                <i class="fa fa-tint" aria-hidden="true"></i>
+                            </div>
+                            <div class="name" data-text="plumbers">Plumbers</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-bolt"></i>
+                                <i class="fas fa-bolt"></i>
+                            </div>
+                            <div class="name" data-text="electricians">Electricians</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-paint-roller"></i>
+                                <i class="fas fa-paint-roller"></i>
+                            </div>
+                            <div class="name" data-text="painters">Painters</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-broom"></i>
+                                <i class="fas fa-broom"></i>
+                            </div>
+                            <div class="name" data-text="cleaning">Cleaning</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row2">
+            <div class="services">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-hammer"></i>
+                                <i class="fas fa-hammer"></i>
+                            </div>
+                            <div class="name" data-text="carpenter">Carpenter</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-fan"></i>
+                                <i class="fas fa-fan"></i>
+                            </div>
+                            <div class="name" data-text="Ac repair">AC Repair</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-tv"></i>
+                                <i class="fas fa-tv"></i>
+                            </div>
+                            <div class="name" data-text="computer repair">Computer Repair</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-skull-crossbones"></i>
+                                <i class="fas fa-skull-crossbones"></i>
+                            </div>
+                            <div class="name" data-text="pest control">Pest control</div>
+                        </a>
+                    </li>
+                
+                    <li>
+                        <a href="#">
+                            <div class="icon">
+                                <i class="fas fa-female"></i>
+                                <i class="fas fa-female"></i>
+                            </div>
+                            <div class="name" data-text="beautician">Beautician</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+    </section>
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/profile-1.jpeg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Prakash and I'm a <span class="typing-2"></span></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut voluptatum eveniet doloremque
+                        autem excepturi eaque, sit laboriosam voluptatem nisi delectus. Facere explicabo hic minus
+                        accusamus alias fuga nihil dolorum quae. Explicabo illo unde, odio consequatur ipsam possimus
+                        veritatis, placeat, ab molestiae velit inventore exercitationem consequuntur blanditiis omnis
+                        beatae. Dolor iste excepturi ratione soluta quas culpa voluptatum repudiandae harum non.</p>
+                    <a href="#">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <!-- <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Design</div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia sunt, quasi quo illo enim.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Advertising</div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia sunt, quasi quo illo enim.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Apps Design</div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia sunt, quasi quo illo enim.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section> -->
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, ratione error est
+                        recusandae consequatur, iusto illum deleniti quidem impedit, quos quaerat quis minima sequi.
+                        Cupiditate recusandae laudantium esse, harum animi aspernatur quisquam et delectus ipsum quam
+                        alias quaerat? Quasi hic quidem illum. Ad delectus natus aut hic explicabo minus quod.</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My teams</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-1.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-2.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-3.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-4.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-5.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos harum corporis fuga
+                        corrupti. Doloribus quis soluta nesciunt veritatis vitae nobis?</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Prakash Shahi</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Surkhet, Nepal</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">abc@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.codingnepalweb.com">CodingNepal</a> | <span
+                class="far fa-copyright"></span> 2020 All rights reserved.</span>
+    </footer>
+
+    
 <!-- Script -->
 
 
